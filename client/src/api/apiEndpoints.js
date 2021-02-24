ab076523d4774b0c6056f59a725a92b892f79d51
Rose <rommims@gmail.com>
2021-02-24 03:27:51 -0800

    endpoint fix for dev

--- client/src/api/apiEndpoints.js
+++ client/src/api/apiEndpoints.js
@@ -1,18 +1,21 @@
 const env = {
   'waterthetrees.com': 'prod',
-  'dev.waterthetrees.com': 'local',
+  'dev.waterthetrees.com': 'dev',
   'blue.waterthetrees.com': 'blue',
   localhost: 'localserver',
+  // localhost: 'docker',
 }[window.location.hostname];
 
-const port = { localserver: 3002 }[env];
+// const port = { localserver: 3002 }[env];
 
 const url = {
   prod: 'https://waterthetrees.com/api',
   dev: 'https://dev.waterthetrees.com/api',
   blue: 'https://blue.waterthetrees.com/api',
-  localserver: `http://localhost:${port}/api`,
+  localserver: 'http://localhost:3002/api',
+  docker: 'http://localhost:3002/api',
 }[env];
+// console.log(env, port, url);
 
 const apiEndpoints = {
   user: `${url}/user`,
--- server/server-dashboard.js
+++ server/server-dashboard.js
@@ -1,38 +1,38 @@
 const http = require('http');
-const https = require('https');
+// const https = require('https');
 const express = require('express');
 
 const app = express();
-const fs = require('fs');
+// const fs = require('fs');
 const morgan = require('morgan');
+// const util = require('util');
 const { logger } = require('../logger.js');
-util = require('util');
 
 // usage: node stripe_webhook_server.js prod or node stripe_webhook_server.js for dev
 const env = process.argv[2] || 'local';
 const host = {
-  dev: 'http://dev.waterthetrees.com',
+  dev: 'http://localhost',
   prod: 'http://localhost',
   blue: 'http://localhost',
   localserver: 'http://localhost',
 }[env];
 const port = {
-  prod: 3001, blue: 3000, dev: 3443, local: 3001, localserver: 3000,
+  prod: 3001, blue: 3000, dev: 3001, local: 3001, localserver: 3000,
 }[env];
 
 app.use(morgan('dev'));
 app.use('/', express.static('client/public'));
 
-const options = {
-  // cert : fs.readFileSync('/etc/letsencrypt/live/dev.100ktrees.com/fullchain.pem'),
-  // key  : fs.readFileSync('/etc/letsencrypt/live/dev.100ktrees.com/privkey.pem'),
-  // cert : fs.readFileSync("./fullchain.pem"),
-  // key  : fs.readFileSync("./privkey.pem"),
-  NPNProtocols: ['http/2.0', 'spdy', 'http/1.1', 'http/1.0'],
-};
+// const options = {
+// cert : fs.readFileSync('/etc/letsencrypt/live/dev.100ktrees.com/fullchain.pem'),
+// key  : fs.readFileSync('/etc/letsencrypt/live/dev.100ktrees.com/privkey.pem'),
+// cert : fs.readFileSync("./fullchain.pem"),
+// key  : fs.readFileSync("./privkey.pem"),
+//   NPNProtocols: ['http/2.0', 'spdy', 'http/1.1', 'http/1.0'],
+// };
 
 const httpServer = http.createServer(app);
-const server = httpServer.listen(port, () => logger.log('info', `${host}:${port}`));
+httpServer.listen(port, () => logger.log('info', `${host}:${port}`));
 
 // const httpsServer = https.createServer(options, app);
 // const server = httpsServer.listen(port, () => logger.log('info', `${host}:${port}`));
