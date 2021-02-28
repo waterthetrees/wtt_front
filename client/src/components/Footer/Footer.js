c09939b40e8d3c7c8ba0c7fd459bb14cfcda58ca
Rose <rommims@gmail.com>
2021-02-28 15:03:06 -0800

    new changes to font

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,14 +5,15 @@ import { Link } from 'react-router-dom';
 const Footer = () => (
   <div className="footer">
     <div className="footer__content">
-
+      <Link to="/contact" className="footer__link">
+        Contact
+      </Link>
       <Link to="/privacy" className="footer__link">
         Privacy Policy
       </Link>
       <Link to="/license" className="footer__link">
         License
       </Link>
-
     </div>
   </div>
 );
