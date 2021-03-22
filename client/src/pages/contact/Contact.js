211e946cb35fd55a3f076e986b61853cd2c90d8f
Rose <rommims@gmail.com>
2021-03-21 19:27:02 -0700

    contactpage

---
+++ client/src/pages/contact/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.scss';
+
+function Contact(props) {
+  console.log('contact', props);
+  return (
+    <div className="contact">
+
+      <div className="contact__header">
+        <h2>Contact</h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default Contact;
