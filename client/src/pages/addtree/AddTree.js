4acb539f97323a5399ce89bb6e569fa046adce4e
zoobot <rommims@gmail.com>
2021-05-21 16:04:06 +0000

    addtree button fix

--- client/src/pages/addtree/AddTree.js
+++ client/src/pages/addtree/AddTree.js
@@ -78,7 +78,6 @@ function AddTree(props) {
         className={cx('addtree__btn', (addTreeSelected) ? 'addtree__btn-selected' : '')}
         onClick={handleOnClick}
       >
-        <img className="addree__plus" src={ADDATREEPLUS} alt="ADD A TREE" />
         <div className="addree__plus-centeredtxt">
           PLANT
         </div>
