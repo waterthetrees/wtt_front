63df924c4d75cd3564cf0e15e59a4ff9ad055f02
Jason Zhen <jasonzhen.mail@gmail.com>
2021-06-27 18:45:11 -0700

    Show 404 error message on dead link

--- client/src/pages/notFound/NotFound.js
+++ client/src/pages/notFound/NotFound.js
@@ -1,12 +1,10 @@
 import React from 'react';
-// import withStyles from 'isomorphic-style-loader/lib/withStyles';
-
-import s from '../../styles/app.scss';
+import './NotFound.scss';
 
 export default function NotFound() {
   return (
-    <div className={s.root}>
-      <h1 className={s.title}>404</h1>
+    <div>
+      <h1 className="title">404</h1>
       <p>Sorry, the page you were trying to view does not exist.</p>
     </div>
   );
