a775c6831a0c947eef977e73d780d8151f83b2d6
simei-yeh <simei.yeh@gmail.com>
2021-06-29 22:25:28 -0700

    added logos and styling for about page

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import './About.scss';
 
+import slackLogo from '../../assets/images/logos/Slack_RGB.svg';
+import googleDocsLogo from '../../assets/images/logos/google-docs.svg';
+
 import Footer from '../../components/Footer';
 
 function About() {
   return (
     <div className="about">
-      <div className="about__p">
-        Water The Trees is a platform that crowd
-        sources tree planting and maintenance. We believe in the power of
-        trees to restore natural habitat for animals, insects, and fauna.
-        We are interested in continuous massive tree planting events
-        to help sequester carbon and stabilize climate change extremes.
+      <div className="about__text">
+        <div className="about__text">
+          Water The Trees is a platform that crowd
+          sources tree planting and maintenance.
+        </div>
+        <div className="about__text">
+          We are an open source project
+          run by tree planting volunteers.
+        </div>
+        <div className="about__text">
+          We believe in the power of
+          trees to restore natural habitat for animals, insects, and fauna.
+        </div>
+        <div className="about__text">
+          We are interested in continuous massive tree planting events
+          to help sequester carbon and stabilize climate change extremes.
+        </div>
       </div>
-      <div className="about__p">
+      <div className="about__links">
         <a
           name="onboarding"
           target="blank"
           href="https://docs.google.com/document/d/1L5Hc8_K_NhVhAejdE05C_Y__CgqeBWFrFYFoNqBSBbQ/edit"
-          className="footer__joinus"
         >
-          Join Us!
+          <img alt="google docs" src={googleDocsLogo} className="about__logo" />
         </a>
-      </div>
-      <div className="about__p">
-        We are an open source project
-        run by tree planting volunteers.
+        <a
+          href="https://join.slack.com/share/zt-ouzg0084-34S7_J9UZlcJSe9~qV7jbQ"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img alt="slack" src={slackLogo} className="about__logo" />
+        </a>
       </div>
       <div className="about__body">
         <div className="about__volunteers">
