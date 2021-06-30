333f3cc1870b536a9e291757fc20ca66e42bbe80
simei-yeh <simei.yeh@gmail.com>
2021-06-29 21:43:43 -0700

    fixed css styling

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -7,22 +7,21 @@ function About() {
   return (
     <div className="about">
       <div className="about__p">
-          Water The Trees is a platform that crowd
-          sources tree planting and maintenance. We believe in the power of
-          trees to restore natural habitat for animals, insects, and fauna.
-          We are interested in continuous massive tree planting events
-          to help sequester carbon
-          and stabilize climate change extremes.
+        Water The Trees is a platform that crowd
+        sources tree planting and maintenance. We believe in the power of
+        trees to restore natural habitat for animals, insects, and fauna.
+        We are interested in continuous massive tree planting events
+        to help sequester carbon and stabilize climate change extremes.
       </div>
       <div className="about__p">
-          <a
-            name="onboarding"
-            target="blank"
-            href="https://docs.google.com/document/d/1L5Hc8_K_NhVhAejdE05C_Y__CgqeBWFrFYFoNqBSBbQ/edit"
-            className="footer__joinus"
-          >
-            Join Us!
-          </a>
+        <a
+          name="onboarding"
+          target="blank"
+          href="https://docs.google.com/document/d/1L5Hc8_K_NhVhAejdE05C_Y__CgqeBWFrFYFoNqBSBbQ/edit"
+          className="footer__joinus"
+        >
+          Join Us!
+        </a>
       </div>
       <div className="about__p">
         We are an open source project
