1d0def7bf1a9ac5a5e1063804f0cae76b632f0a5
Rose <rommims@gmail.com>
2021-07-14 16:37:09 -0700

    topTrees food, SF, natives

---
+++ client/src/pages/addtree/topTreesSanFrancisco.js
@@ -0,0 +1,462 @@
+// https://www.fuf.net/resources-reference/urban-tree-species-directory/
+
+export const topTreesSanFrancisco = [
+  {
+    common: 'American Sweet Gum',
+    scientific: 'Liquidambar styraciflua ‘Rotundiloba’/ ‘Palo Alto’/ ‘Burgundy’/ ‘Festival’',
+    genus: 'Liquidambar',
+  },
+  {
+    common: 'Australian Tea Tree',
+    scientific: 'Leptospermum laevigatum',
+    genus: 'Leptospermum',
+  },
+  {
+    common: 'Australian Willow',
+    scientific: 'Geijera parviflora',
+    genus: 'Geijera',
+  },
+  {
+    common: 'Autumn Blaze',
+    scientific: 'Acer x freemanii ‘Autumn Blaze‘',
+    genus: 'Acer',
+  },
+  {
+    common: 'Bailey’s Acacia',
+    scientific: 'Acacia baileyana',
+    genus: 'Acacia',
+  },
+  {
+    common: 'Black Acacia',
+    scientific: 'Acacia melanoxylon',
+    genus: 'Acacia',
+  },
+  {
+    common: 'Brazilian Pepper Tree',
+    scientific: 'Schinus terebinthifolia',
+    genus: 'Schinus',
+  },
+  {
+    common: 'Brisbane Box',
+    scientific: 'Lophostemon confertus (formerly Tristania conferta)',
+    genus: 'Lophostemon',
+  },
+  {
+    common: 'Bronze Loquat',
+    scientific: 'Eriobotrya deflexa',
+    genus: 'Eriobotrya',
+  },
+  {
+    common: 'Cajeput Tree',
+    scientific: 'Melaleuca quinquenervia',
+    genus: 'Melaleuca',
+  },
+  {
+    common: 'California Buckeye',
+    scientific: 'Aesculus californica',
+    genus: 'Aesculus',
+  },
+  {
+    common: 'California Lilac',
+    scientific: 'Ceanothus ‘Ray Hartman’',
+    genus: 'Ceanothus',
+  },
+  {
+    common: 'Camphor Tree',
+    scientific: 'Cinnamomum camphora',
+    genus: 'Cinnamomum',
+  },
+  {
+    common: 'Carob Tree',
+    scientific: 'Ceratonia siliqua',
+    genus: 'Ceratonia',
+  },
+  {
+    common: 'Carrotwood',
+    scientific: 'Cupaniopsis anacardioides',
+    genus: 'Cupaniopsis',
+  },
+  {
+    common: 'Catalina Cherry',
+    scientific: 'Prunus ilicifolia ssp. lyonii',
+    genus: 'Prunus',
+  },
+  {
+    common: 'Champaca Magnolia',
+    scientific: 'champaca (Michelia champaca)',
+    genus: 'champaca',
+  },
+  {
+    common: 'Chinese Elm Ulmus',
+    scientific: 'parvifolia ‘Drake’ / ‘Sempervirens’ / ‘True Green’',
+    genus: 'parvifolia',
+  },
+  {
+    common: 'Chinese Flame Tree',
+    scientific: 'Koelreuteria bipinnata',
+    genus: 'Koelreuteria',
+  },
+  {
+    common: 'Chinese Pistache',
+    scientific: 'Pistacia chinensis',
+    genus: 'Pistacia',
+  },
+  {
+    common: 'Coast Live Oak',
+    scientific: 'Quercus agrifolia',
+    genus: 'Quercus',
+  },
+  { common: 'Cork Oak', scientific: 'Quercus suber', genus: 'Quercus' },
+  {
+    common: 'Crepe Myrtle',
+    scientific: 'Lagerstroemia ‘Tuscarora’/ ‘Natchez’',
+    genus: 'Lagerstroemia',
+  },
+  {
+    common: 'Daybreak Cherry',
+    scientific: 'Prunus x yedoensis ‘Akebono’',
+    genus: 'Prunus',
+  },
+  {
+    common: 'Dracaena Palm',
+    scientific: 'Cordyline australis',
+    genus: 'Cordyline',
+  },
+  {
+    common: 'English Hawthorn',
+    scientific: 'Crataegus laevigata',
+    genus: 'Crataegus',
+  },
+  {
+    common: 'European White',
+    scientific: 'Birch Betula pendula',
+    genus: 'Birch',
+  },
+  {
+    common: 'Evergreen Ash',
+    scientific: 'Fraxinus uhdei',
+    genus: 'Fraxinus',
+  },
+  {
+    common: 'Evergreen Pear',
+    scientific: 'Pyrus kawakamii',
+    genus: 'Pyrus',
+  },
+  {
+    common: 'Fern Pine',
+    scientific: 'Afrocarpus gracilior (formerly Podocarpus gracilior)',
+    genus: 'Afrocarpus',
+  },
+  {
+    common: 'Fernleaf Catalina Ironwood',
+    scientific: 'Lyonothamnus floribundus asplenifolius',
+    genus: 'Lyonothamnus',
+  },
+  {
+    common: 'Flaxleaf Paperbark',
+    scientific: 'Melaleuca linariifolia',
+    genus: 'Melaleuca',
+  },
+  {
+    common: 'Flowering Cherry',
+    scientific: 'Prunus serrulata ‘Kwanzan’',
+    genus: 'Prunus',
+  },
+  {
+    common: 'Flowering Pear',
+    scientific: 'Pyrus calleryana ‘Chanticleer’/ ‘New Bradford’/ ‘Aristocrat’',
+    genus: 'Pyrus',
+  },
+  {
+    common: 'Flowering Plum',
+    scientific: 'Prunus x blireana',
+    genus: 'Prunus',
+  },
+  {
+    common: 'Glossy Privet',
+    scientific: 'Ligustrum lucidum',
+    genus: 'Ligustrum',
+  },
+  {
+    common: 'Gold Medallion Tree',
+    scientific: 'Cassia leptophylla',
+    genus: 'Cassia',
+  },
+  {
+    common: 'Golden Rain Tree',
+    scientific: 'Koelreuteria paniculata',
+    genus: 'Koelreuteria',
+  },
+  {
+    common: 'Griselinia',
+    scientific: 'Griselinia littoralis',
+    genus: 'Griselinia',
+  },
+  {
+    common: 'Heath Melaleuca',
+    scientific: 'Melaleuca ericifolia',
+    genus: 'Melaleuca',
+  },
+  {
+    common: 'Honeylocust',
+    scientific: 'Gleditsia triacanthos var. inermis ‘Shademaster’',
+    genus: 'Gleditsia',
+  },
+  {
+    common: 'Italian Stone Pine',
+    scientific: 'Pinus pinea',
+    genus: 'Pinus',
+  },
+  {
+    common: 'Jacaranda',
+    scientific: 'Jacaranda mimosifolia',
+    genus: 'Jacaranda',
+  },
+  {
+    common: 'Japanese Blueberry Tree',
+    scientific: 'Elaeocarpus decipiens',
+    genus: 'Elaeocarpus',
+  },
+  {
+    common: 'Japanese Flowering Crabapple',
+    scientific: 'Malus floribunda ‘Prarie Fire’',
+    genus: 'Malus',
+  },
+  {
+    common: 'Japanese Maple',
+    scientific: 'Acer palmatum',
+    genus: 'Acer',
+  },
+  {
+    common: 'Karo',
+    scientific: 'Pittosporum crassifolium',
+    genus: 'Pittosporum',
+  },
+  {
+    common: 'Kohuhu',
+    scientific: 'Black Pittosporum Pittosporum tenuifolium',
+    genus: 'Black',
+  },
+  {
+    common: 'Lemon Bottlebrush',
+    scientific: 'Callistemon citrinus',
+    genus: 'Callistemon',
+  },
+  {
+    common: 'Lemonwood Tree',
+    scientific: 'Pittosporum eugeniodes',
+    genus: 'Pittosporum',
+  },
+  { common: 'Linden', scientific: 'Tilia cordata', genus: 'Tilia' },
+  {
+    common: 'London Plane Tree',
+    scientific: 'Platanus x hispanica ‘Columbia’/ ‘Yarwood’/ ‘Bloodgood’',
+    genus: 'Platanus',
+  },
+  {
+    common: 'Maidenhair Tree',
+    scientific: 'Ginkgo biloba',
+    genus: 'Ginkgo',
+  },
+  {
+    common: 'Mayten',
+    scientific: 'Maytenus boaria ‘Green Showers’',
+    genus: 'Maytenus',
+  },
+  {
+    common: 'Mediterranean Fan Palm',
+    scientific: 'Chamaerops humilis',
+    genus: 'Chamaerops',
+  },
+  {
+    common: 'Mountain She-Oak',
+    scientific: 'Beefwood Allocasuarina verticillata',
+    genus: 'Beefwood',
+  },
+  {
+    common: 'Myoporum',
+    scientific: 'Myoporum laetum \'Clean n Green\'',
+    genus: 'Myoporum',
+  },
+  {
+    common: 'New Zealand Christmas Tree',
+    scientific: 'Metrosideros excelsa',
+    genus: 'Metrosideros',
+  },
+  {
+    common: 'New Zealand Tea Tree',
+    scientific: 'Leptospermum scoparium',
+    genus: 'Leptospermum',
+  },
+  {
+    common: 'Olive Tree',
+    scientific: 'Olea europaea ‘Majestic Beauty’/ ‘Fruitless’/ ‘Swan Hill’',
+    genus: 'Olea',
+  },
+  {
+    common: 'Oriental Sweetgum',
+    scientific: 'Liquidambar orientalis',
+    genus: 'Liquidambar',
+  },
+  {
+    common: 'Pepper Tree',
+    scientific: 'Schinus molle',
+    genus: 'Schinus',
+  },
+  {
+    common: 'Peppermint Willow',
+    scientific: 'Agonis flexuosa',
+    genus: 'Agonis',
+  },
+  {
+    common: 'Pink Melaleuca',
+    scientific: 'Melaleuca nesophila',
+    genus: 'Melaleuca',
+  },
+  {
+    common: 'Primrose Tree',
+    scientific: 'Lagunaria patersonii',
+    genus: 'Lagunaria',
+  },
+  {
+    common: 'Purple Robe Locust',
+    scientific: 'Robinia x ambigua \'Purple Robe\'',
+    genus: 'Robinia',
+  },
+  {
+    common: 'Purple-Leaf Plum',
+    scientific: 'Prunus cerasifera ‘Krauter Vesuvius’',
+    genus: 'Prunus',
+  },
+  {
+    common: 'Queen Palm',
+    scientific: 'Syagrus romanzoffiana',
+    genus: 'Syagrus',
+  },
+  {
+    common: 'Raywood Ash',
+    scientific: 'Fraxinus angustifolia \'Raywood\'',
+    genus: 'Fraxinus',
+  },
+  {
+    common: 'Red Horsechestnut',
+    scientific: 'Aesculus carnea',
+    genus: 'Aesculus',
+  },
+  {
+    common: 'Red Ironbark',
+    scientific: 'Eucalyptus sideroxylon ‘Rosea’',
+    genus: 'Eucalyptus',
+  },
+  { common: 'Red Maple', scientific: 'Acer rubrum', genus: 'Acer' },
+  {
+    common: 'Red-flowering Gum',
+    scientific: 'Corymbia ficifolia',
+    genus: 'Corymbia',
+  },
+  {
+    common: 'River Birch',
+    scientific: 'Betula nigra',
+    genus: 'Betula',
+  },
+  {
+    common: 'Saucer Magnolia',
+    scientific: 'Magnolia x soulangiana',
+    genus: 'Magnolia',
+  },
+  {
+    common: 'Scarlet Oak',
+    scientific: 'Quercus coccinea',
+    genus: 'Quercus',
+  },
+  {
+    common: 'Silver Dollar Gum',
+    scientific: 'Eucalyptus polyanthemos',
+    genus: 'Eucalyptus',
+  },
+  {
+    common: 'Small-Leaf Tristania',
+    scientific: 'Tristaniopsis laurina (formerly Tristania laurina)',
+    genus: 'Tristaniopsis',
+  },
+  {
+    common: 'Soapbark Tree',
+    scientific: 'Quillaja saponaria',
+    genus: 'Quillaja',
+  },
+  {
+    common: 'Southern Magnolia',
+    scientific: 'Magnolia grandiflora',
+    genus: 'Magnolia',
+  },
+  {
+    common: 'Strawberry Tree',
+    scientific: 'Arbutus \'Marina\'',
+    genus: 'Arbutus',
+  },
+  {
+    common: 'Sweet Bay Laurus',
+    scientific: 'nobilis \'Saratoga\'',
+    genus: 'nobilis',
+  },
+  {
+    common: 'Sweet Hakea',
+    scientific: 'Hakea suaveolens',
+    genus: 'Hakea',
+  },
+  {
+    common: 'Sweet Michelia Magnolia',
+    scientific: 'doltsopa (Michelia doltsopa)',
+    genus: 'doltsopa',
+  },
+  {
+    common: 'Sweetshade',
+    scientific: 'Hymenosporum flavum',
+    genus: 'Hymenosporum',
+  },
+  {
+    common: 'Trident Maple',
+    scientific: 'Acer buergeranum',
+    genus: 'Acer',
+  },
+  {
+    common: 'Victorian Box',
+    scientific: 'Pittosporum undulatum',
+    genus: 'Pittosporum',
+  },
+  {
+    common: 'Vine Maple',
+    scientific: 'Acer circinatum',
+    genus: 'Acer',
+  },
+  {
+    common: 'Washington Hawthorn',
+    scientific: 'Crataegus phaenopyrum',
+    genus: 'Crataegus',
+  },
+  {
+    common: 'Weeping Bottlebrush',
+    scientific: 'Callistemon viminalis',
+    genus: 'Callistemon',
+  },
+  {
+    common: 'Western Redbud',
+    scientific: 'Cercis occidentalis',
+    genus: 'Cercis',
+  },
+  {
+    common: 'White Ash',
+    scientific: 'Fraxinus americana',
+    genus: 'Fraxinus',
+  },
+  {
+    common: 'White Mulberry',
+    scientific: 'Morus alba “fruitless”',
+    genus: 'Morus',
+  },
+  {
+    common: 'Windmill Palm',
+    scientific: 'Trachycarpus fortune',
+    genus: 'Trachycarpus',
+  },
+];
