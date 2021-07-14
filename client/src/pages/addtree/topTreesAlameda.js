9672951dd341234032f5c74f87b39b3b9b7afaa3
Rose <rommims@gmail.com>
2021-07-13 23:30:42 -0700

    test data for top tree filters taken from FUF and Alameda master street tree plan

---
+++ client/src/pages/addtree/topTreesAlameda.js
@@ -0,0 +1,122 @@
+export const topTrees = [
+  {
+    common: 'Vacant Site',
+    scientific: 'Vacant Site',
+    genus: 'Vacant Site',
+    city: 'Alameda',
+    plantingStrip: '',
+    neighborhood: 'West End',
+  },
+  {
+    neighborhood: 'BALLENA BAY',
+    notes: `Street trees in this neighborhood are limited to Ballena Boulevard, Tideway, and Cola Ballena. 
+       This neighborhood was not surveyed in the 2008 tree inventory.`,
+    plantingRecommendations:
+    [
+      {
+        scientific: 'Carpinus betulus ‘Fastigiata’',
+        common: 'fastigiate hornbeam',
+        genus: 'Carpinus',
+      },
+      {
+        scientific: 'Metrosideros excelsus',
+        common: 'New Zealand Christmas tree',
+        genus: 'Metrosideros',
+      },
+    ],
+  },
+  {
+    neighborhood: 'BAY FARM ISLAND',
+    treesSurveyed: 3720,
+    vacantSites: 455,
+    plantingStripWidths: '2-7 feet',
+    notes: `Maintenance in this area is currently being shared by the homeowners’ association and the City. 
+    In many of the new developments, trees are being planted at a spacing pattern far too dense for the 
+    eventual mature dimensions of these trees. 
+    Future thinning out of these trees may be necessary in order to maintain the overall health of this sub-population.`,
+    plantingRecommendations: [
+      { scientific: 'Acer campestre', common: 'Hedge maple', genus: 'Acer' },
+      { scientific: 'Tristania laurina', common: 'swamp myrtle', genus: 'Tristania' },
+      { scientific: 'Metrosideros  excelsus ', common: ' New  Zealand  Christmas  tree', genus: 'Metrosideros' },
+      { scientific: 'Aesculus  hippocastanum‘Baumannii’', common: 'Baumann’s horsechestnut', genus: 'Aesculus' },
+      { scientific: 'Corymbia ficifolia', common: 'red flowering gum', genus: 'Corymbia' },
+      { scientific: 'Jacaranda mimosifolia', common: 'Jacaranda', genus: 'Jacaranda' },
+      { scientific: 'Quercus coccinea', common: 'scarlet oak', genus: 'Quercus' },
+      { scientific: 'Quercus palustris', common: 'pin oak', genus: 'Quercus' },
+      { scientific: 'Quercus suber', common: 'cork oak', genus: 'Quercus' },
+    ],
+  },
+  {
+    neighborhood: 'BAY MOUND',
+    treesSurveyed: 1227,
+    vacantSites: 429,
+    plantingStripWidths: '2-7 feet',
+    notes: `Five species comprise the majority of the tree population in this neighborhood. 
+    These trees are distributed fairly evenly between Camphor, Myoporum, Carolina Cherry, and Carob. 
+    Unfortunately, many of these species are notorious for uplifting sidewalks; 
+    they are also the species which will mature when they reach heights of 30 feet or less. 
+    The Camphor are mostly 12-18” diameter at breast height (DBH). 
+    Overall, the majority of the trees in the neighborhood are 3-6” DBH, indicating a young population. 
+    Because there are vacant planting sites available, there are many opportunities to change species diversity.
+    New tree species could be planted, establishing a significant tree canopy in anticipation of 
+    removing the more destruc-tive species such as Camphor, Carob, and Indian Laurel Fig.`,
+    plantingRecommendations: [
+      { scientific: 'Acer buergeranum', common: 'trident maple', genus: 'Acer' },
+      { scientific: 'Acer campestre', common: 'hedge maple', genus: 'Acer' },
+      { scientific: 'Aesculus carnea', common: 'red horsechestnut', genus: 'Aesculus' },
+      { scientific: 'Corymbia ficifolia', common: 'red-flowering gum', genus: 'Corymbia' },
+      { scientific: 'Metrosideros excelsus', common: 'New Zealand Christmas tree', genus: 'Metrosideros' },
+      { scientific: 'Acer saccharum ‘Commemoration’', common: 'sugar maple', genus: 'Acer' },
+      { scientific: 'Carpinus betulus ‘Fastigiata’ and ‘Frans ‘Fontaine’', common: 'hornbeam Pistacia', genus: 'Carpinus' },
+      { scientific: 'chinesis Jacaranda mimosifolia', common: 'Jacaranda', genus: 'Jacaranda' },
+      { scientific: 'Quercus palustris', common: 'pin oak', genus: 'Quercus' },
+      { scientific: 'Ulmus americana ‘Princeton’', common: 'elm', genus: 'Ulmus' },
+    ],
+  },
+  {
+    neighborhood: 'BAY VIEW',
+    treesSurveyed: 59,
+    vacantSites: 24,
+    plantingStripWidths: '3-7 feet',
+    notes: 'The public tree population in the Bayview area is limited due to lack of public easement.',
+    plantingRecommendations: [
+      { scientific: 'Acer buergeranum', common: 'trident maple', genus: 'Acer' },
+      { scientific: 'Acer campestre', common: 'hedge maple', genus: 'Acer' },
+      { scientific: 'Aesculus carnea', common: 'red horsechestnut', genus: 'Aesculus' },
+      { scientific: 'Corymbia ficifolia', common: 'red-flowering gum', genus: 'Corymbia' },
+      { scientific: 'Metrosideros excelsus', common: 'New Zealand Christmas tree', genus: 'Metrosideros' },
+      { scientific: 'Acer saccharum ‘Commemoration’', common: 'sugar maple', genus: 'Acer' },
+      { scientific: 'Carpinus betulus ‘Fastigiata’ and ‘Frans ‘Fontaine’', common: 'hornbeam Pistacia', genus: 'Carpinus' },
+      { scientific: 'chinesis Jacaranda mimosifolia', common: 'Jacaranda', genus: 'Jacaranda' },
+      { scientific: 'Quercus palustris', common: 'pin oak', genus: 'Quercus' },
+      { scientific: 'Ulmus americana ‘Princeton’', common: 'elm', genus: 'Ulmus' },
+    ],
+  },
+  {
+    neighborhood: 'CENTRAL',
+    treesSurveyed: 384,
+    vacantSites: 118,
+    plantingStripWidths: '2-6 feet',
+    notes: `Excellent, deep soils and many mature trees typify this neighborhood. 
+    Many of the older trees are species that cause considerable concrete damage. 
+    Most trees are in the 6-12” and 12-18” DBH classes. 
+    Seventy percent of the planting sites are 3.5 feet wide, 
+    which is a fairly large space compared to the common planting sizes in other parts of the city. 
+    The main east/west streets are discussed in the “Major Streets” section of this report. 
+    The recommendations made here are meant for the comparatively narrow north/south streets.`,
+    plantingRecommendations: [
+      { scientific: 'Acer buergeranum', common: 'trident maple', genus: 'Acer' },
+      { scientific: 'Acer campestre', common: 'hedge maple', genus: 'Acer' },
+      { scientific: 'Aesculus carnea', common: 'red horsechestnut', genus: 'Aesculus' },
+      { scientific: 'Corymbia ficifolia', common: 'red-flowering gum', genus: 'Corymbia' },
+      { scientific: 'Ginkgo biloba ‘Saratoga’', common: 'Saratoga maidenhair tree' },
+      { scientific: 'Koelreuteria bipinnata', common: 'Chinese flame tree' },
+      { scientific: 'Podocarpus gracilior', common: 'fern pine' },
+      { scientific: 'Tilia tomentosa ‘Green Mountain’ & ‘Sterling’', common: 'silver linden' },
+      { scientific: 'Ginkgo biloba ‘Saratoga’', common: 'Saratoga maidenhair tree' },
+      { scientific: 'Koelreuteria bipinnata', common: 'Chinese flame tree' },
+      { scientific: 'Podocarpus gracilior', common: 'fern pine' },
+      { scientific: 'Tilia tomentosa ‘Green Mountain’ & ‘Sterling’', common: 'silver linden' },
+    ],
+  },
+];
