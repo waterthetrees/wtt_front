d06d583fb0d30a3f0d24fdc78d0972a624c4cc2a
Jason Zhen <jasonzhen.mail@gmail.com>
2021-07-13 22:16:48 -0700

    change from vanilla css to Material UI

--- client/src/pages/notFound/NotFound.js
+++ client/src/pages/notFound/NotFound.js
@@ -1,11 +1,22 @@
+import { Grid } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
-import './NotFound.scss';
+import Footer from '../../components/Footer/Footer';
 
 export default function NotFound() {
   return (
-    <div>
-      <h1 className="title">404</h1>
-      <p>Sorry, the page you were trying to view does not exist.</p>
-    </div>
+    <Grid container spacing={2} justify="center" alignItems="center" direction="column">
+      <Grid item>
+        <Typography variant="h1">
+          404
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="subtitle1">
+          Sorry, the page you were trying to view does not exist.
+        </Typography>
+      </Grid>
+      <Footer />
+    </Grid>
   );
 }
