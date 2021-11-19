d08db130971aeaeb38ed7e7f489fa50971292a6d
John Dunning <fw@johndunning.com>
2021-11-18 18:20:34 -0800

    Remove draft.js
    The library doesn't seem to be used, so this saves about ~130KB from the bundle.
    Remove the earth icon from the add tree dialog, since it's right next to buttons but isn't a button.
    Remove unnecessary state from AddTreeModal.js.

--- client/src/pages/addtree/ButtonsResult.js
+++ client/src/pages/addtree/ButtonsResult.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import { convertToRaw } from 'draft-js';
 
-const EARTH = 'assets/images/addtree/earth2.svg';
 export default ({
-  data, reset, defaultValues, setAddTreeSelected,
+  reset, defaultValues, setAddTreeSelected,
 }) => (
   <div className="addtree_btngroup">
-    {data && (
-      <pre>
-        {JSON.stringify(
-          {
-            ...data,
-            DraftJS: convertToRaw(data.DraftJS.getCurrentContent()).blocks[0]
-              .text,
-          },
-          null,
-          2,
-        )}
-      </pre>
-    )}
-    <img
-      alt="EARTH"
-      className="addtreemodal__header-image"
-      src={EARTH}
-    />
     <button
       className="addtree_btn__save button buttonBlack btn btn-secondary btn-lg"
       type="button"
