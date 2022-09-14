733eab56016a495f24336463b6e0383dfde7a1cf
Matthew Pereira <matthewdavidpereira@gmail.com>
2022-09-14 14:23:37 -0700

    redesign 404 page

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,30 +1,49 @@
 import React from 'react';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Button, Container } from '@mui/material';
 import { Footer } from '@/components/Footer/Footer';
+import './NotFound.scss';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      direction="column"
-      sx={{ height: '100vh' }}
-    >
-      <Grid item>
-        <Typography
-          variant="h1"
-          sx={{ my: 2 }}
+    <div className="NotFound">
+      <div className="background-overlay"></div>
+      <Container>
+        <Grid
+          container
+          justifyContent={{ md: 'center', sm: 'flex-start' }}
+          alignItems="flex-start"
+          direction="column"
+          sx={{ height: '100vh' }}
         >
-          404
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Typography variant="subtitle1">
-          Sorry, the page you were trying to view does not exist.
-        </Typography>
-      </Grid>
-      <Footer />
-    </Grid>
+          <Grid item>
+            <Typography variant="h1">404</Typography>
+          </Grid>
+          <Grid item sx={{ mb: 5 }}>
+            <Typography paragraph>
+              Sorry, we can&apos;t find that page! Don&apos;t worry though,
+              there are still plenty of trees to water!
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="outlined"
+              size="large"
+              sx={{
+                color: 'white',
+                backgroundColor: 'black',
+                border: '2px solid white',
+                fontSize: '1.5rem',
+                fontWeight: '800',
+              }}
+            >
+              <Link to="/">water the trees</Link>
+            </Button>
+          </Grid>
+          <Footer />
+        </Grid>
+        <Footer />
+      </Container>
+    </div>
   );
 }
