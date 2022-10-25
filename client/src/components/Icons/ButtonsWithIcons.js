16d5d6c0bdf22ce4e210e9bbcb347937eb1b87ae
vernfongchao <Vernchao93@gmail.com>
2022-10-24 18:04:01 -0700

    community buttons styled

--- client/src/components/Icons/ButtonsWithIcons.js
+++ client/src/components/Icons/ButtonsWithIcons.js
@@ -1,38 +1,30 @@
 import React from 'react';
 import { UploadFile, Link, LinkOff } from '@mui/icons-material';
+import {Button} from '@mui/material'
+
+import { styled } from '@mui/material/styles';
 
 
 
+export const CommunityButton = (props) => {
 
-export const ExportButton = (props) => {
+    const ExportButton = styled(Button)({
+        backgroundColor: 'white',
+        borderRadius:'.3vw',
+        border: '1px solid #00000050',
+        color: '#00000050',
+        height: '32px'
+    })
+
+
+
     return(
-        <button
-            // onClick={props.handleclick}
+        <ExportButton
+            onClick={props.handleclick}
         >
-            <UploadFile sx={{ color: '#00000050', verticalAlign: 'top' }} {...props} />
+            <UploadFile sx={{ color: '#00000050', verticalAlign: 'top', marginRight:'.4vw' }} {...props} />
             {props.text}
-        </button>
+        </ExportButton>
     )
 }
 
-export const LinkButton = (props) => {
-    return (
-        <button
-            onClick={props.handleclick}
-        >
-            <Link sx={{ color: '#00000050', verticalAlign: 'top' }} {...props} />
-            {props.text}
-        </button>
-    )
-}
-
-export const ReportLinkButton = (props) => {
-    return (
-        <button
-            onClick={props.handleclick}
-        >
-            <LinkOff sx={{ color: '#00000050', verticalAlign: 'top' }} {...props} />
-            {props.text}
-        </button>
-    )
-}
