34f2331c3cd5f31d1c146236b2d52416973c76e9
vernfongchao <Vernchao93@gmail.com>
2022-10-26 14:13:34 -0700

    linted components associated with communities and community pages

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,60 +1,51 @@
-import React from "react";
-import { Button } from '@mui/material'
+import React from 'react';
+import { Button } from '@mui/material';
 
 import { styled } from '@mui/material/styles';
 
-
 export const GreenButton = (props) => {
+  const Green = styled(Button)({
+    backgroundColor: '#147d16',
+    borderRadius: '.3vw',
+    color: 'white',
+    height: '32px',
+  });
 
-    const Green = styled(Button)({
-        backgroundColor: '#147d16',
-        borderRadius: '.3vw',
-        color: 'white',
-        height: '32px',
-    })
-
-    return (
-        <Green type={props.type}
-            sx={props?.sx}
-            onClick={props.onClick}
-        >
-            {props.children}
-        </Green>
-    )
-}
+  return (
+    <Green type={props.type} sx={props?.sx} onClick={props.onClick}>
+      {props.children}
+    </Green>
+  );
+};
 
 export const WhiteButton = (props) => {
-    const White = styled(Button)({
-        backgroundColor: 'white',
-        border: '1px #147d16 solid',
-        borderRadius: '.3vw',
-        color: '#00000050',
-        height: '32px',
-    })
+  const White = styled(Button)({
+    backgroundColor: 'white',
+    border: '1px #147d16 solid',
+    borderRadius: '.3vw',
+    color: '#00000050',
+    height: '32px',
+  });
 
-    return (
-        <White type={props.type} onClick={props.onClick} sx={props?.sx}>
-            {props.children}
-        </White>
-    )
-}
+  return (
+    <White type={props.type} onClick={props.onClick} sx={props?.sx}>
+      {props.children}
+    </White>
+  );
+};
 
 export const GrayButton = (props) => {
-    const Gray = styled(Button)({
-        backgroundColor: 'white',
-        borderRadius: '.3vw',
-        border: '1px solid #00000050',
-        color: '#00000050',
-        height: '32px'
-    })
+  const Gray = styled(Button)({
+    backgroundColor: 'white',
+    borderRadius: '.3vw',
+    border: '1px solid #00000050',
+    color: '#00000050',
+    height: '32px',
+  });
 
-    return (
-        <Gray
-            type={props.type}
-            onClick={props.onClick}
-            sx={props?.sx}
-        >
-            {props.children}
-        </Gray>
-    )
-}
+  return (
+    <Gray type={props.type} onClick={props.onClick} sx={props?.sx}>
+      {props.children}
+    </Gray>
+  );
+};
--- client/src/components/Icons/Upload.js
+++ client/src/components/Icons/Upload.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { UploadFile } from '@mui/icons-material';
 
-export const UploadFileIcon = (props) => {
-    return (
-        <>
-            <UploadFile sx={{ verticalAlign: 'top' }} {...props} />
-        </>
-    )
-}
+export const UploadFileIcon = (props) => (
+  <UploadFile sx={{ verticalAlign: 'top' }} {...props} />
+);
