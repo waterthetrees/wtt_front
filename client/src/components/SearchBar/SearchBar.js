82a8fb12dc148d1430ff68a237dde7b13bd1ec81
vernfongchao <Vernchao93@gmail.com>
2022-10-26 13:39:05 -0700

    ESLint formating for side menu

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React from 'react';
 
-import {Search} from '@/components/Icons/Search'
+import { Search } from '@/components/Icons/Search';
 
-import './SearchBar.scss'
+import './SearchBar.scss';
 export const SearchBar = (props) => {
+  return (
+    <div className="searchbar" style={{ ...props.style.div }}>
+      <Search
+        sx={{
+          color: '#00000050',
+          fontSize: '24px',
+          marginLeft: '10px',
+          marginRight: '10px',
+          fontFamily: 'Montserrat',
+        }}
+      />
 
-    return (
-        <div className="searchbar"
-            style={{ ...props.style.div }}
-        >
-            <Search sx={{ color: '#00000050', fontSize: '24px', marginLeft: '10px', marginRight: '10px', fontFamily: 'Montserrat' }} />
-
-            <input
-                style={{ ...props.style.input }}
-                value={props.search}
-                onChange={props.onChange}
-                placeholder={props.placeholder}
-                className="searchbar__input"
-            />
-        </div>
-    )
-
-}
-
-
+      <input
+        style={{ ...props.style.input }}
+        value={props.search}
+        onChange={props.onChange}
+        placeholder={props.placeholder}
+        className="searchbar__input"
+      />
+    </div>
+  );
+};
