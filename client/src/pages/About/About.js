a8abfd8aa450e45f56ff3a39980597c7e509b64d
vernfongchao <Vernchao93@gmail.com>
2022-11-08 16:18:16 -0800

    About Page changed to styled components

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
 import { Footer } from '@/components/Footer/Footer';
 import { Affiliates } from '@/components/Contacts';
-
 import { TreeLogo } from '@/components/Icons';
 
 import TreeImage from '@/assets/images/addtree/treefattrunk.png';
@@ -10,72 +10,158 @@ import TreeImage from '@/assets/images/addtree/treefattrunk.png';
 import './About.scss';
 
 const About = () => {
-  return (
-    <div className="about">
-      <div className="about__header">
-        <div className="about__header__logo">
-          <TreeLogo />
-        </div>
-        <h1 className="about__header__text">About Water the Trees</h1>
-        <span className="about__header__summary">
-          Welcome to Water the Trees, a platform that crowd sources tree
-          planting and
-        </span>
-        <span className="about__header__summary">
-          maintenance. We are an open source project run by tree planting
-          volunteers.
-        </span>
-      </div>
-      <div className="about__main">
-        <div className="about__main__section">
-          <h2 className="about__main__section__header">Vision</h2>
-          <p>
-            We believe in the power of trees to restore natural habitat for
-            animals, insect, and
-          </p>
-          <p>fauna.</p>
-        </div>
-        <div className="about__main__section">
-          <h2 className="about__main__section__header">Mission</h2>
-          <p>
-            We are interested in continuous massive tree planting events to help
-            sequester
-          </p>
-          <p>carbon footprint and stablize climate change extremes</p>
-        </div>
-        <div className="about__main__section">
-          <h2 className="about__main__section__header">Who we are!</h2>
-          <p>
-            Water the Trees begun as a tree tracking platform to track the life
-            and health of a
-          </p>
-          <p>tree worldwide.</p>
-          <div className="about__main__section__subsection">
-            <p>
-              Together with data, three enthusiast, and volunteers we are able
-              to maintain a
-            </p>
-            <p>source to see the growth of trees in all communities.</p>
-          </div>
-        </div>
+  const PageContainer = styled.div({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    boxSizing: 'border-box',
+    fontFamily: 'Montserrat',
+    margin: '72px auto 38px auto',
+    // marginTop: '72px',
+    // marginBottom: '38px',
+    textAlign: 'center',
+    fontSize: '1.33rem',
+    '@media(max-width: 768px)': {
+      margin: '0px',
+      padding: '3rem 2rem',
+    },
+    a: {
+      textDecoration: 'underline',
+      color: '#3FAB45',
+      cursor: 'pointer',
+    },
+  });
 
-        <div className="about__main__section">
-          <h2 className="about__main__section__header">Join the team!</h2>
-          <p>
+  const HeaderContainer = styled.div({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '112px',
+    marginBottom: '56px',
+    width: '30%',
+    '@media(max-width: 768px)': {
+      width: '100%',
+    },
+  });
+
+  const HeaderLogoContainer = styled.div({
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: '24px',
+  });
+
+  const HeaderText = styled.h1({
+    fontSize: '2.65rem',
+    marginBottom: '8px',
+  });
+
+  const HeaderSummary = styled.span({});
+
+  const MainContainer = styled.div({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '28%',
+    '@media(max-width: 768px)': {
+      width: '100%',
+    },
+  });
+
+  const MainSection = styled.div({
+    marginBottom: '40px',
+    'p:last-child': {
+      marginBottom: '0px',
+    },
+  });
+
+  const MainSectionHeader = styled.h2({});
+
+  const MainSectionSummary = styled.p({
+    marginBottom: '16px',
+  });
+
+  const MainSubSection = styled.div({
+    marginTop: '24px',
+    p: {
+      marginBottom: '0px',
+    },
+  });
+
+  const MainSubSectionHeader = styled.h4({});
+
+  const FooterContainer = styled.div({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#EFEFEF',
+  });
+
+  const AffiliatesContainer = styled.div({
+    width: '30%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: '24px',
+    img: {
+      height: '64px',
+    },
+    '@media(max-width: 768px)': {
+      width: '100%',
+    },
+  });
+
+  return (
+    <PageContainer>
+      <HeaderContainer>
+        <HeaderLogoContainer>
+          <TreeLogo />
+        </HeaderLogoContainer>
+        <HeaderText>About Water the Trees</HeaderText>
+        <HeaderSummary>
+          Welcome to Water the Trees, a platform that crowd sources tree
+          planting and maintenance. We are an open source project run by tree
+          planting volunteers.
+        </HeaderSummary>
+      </HeaderContainer>
+      <MainContainer></MainContainer>
+      <MainContainer>
+        <MainSection>
+          <MainSectionHeader>Vision</MainSectionHeader>
+          <MainSectionSummary>
+            We believe in the power of trees to restore natural habitat for
+            animals, insect, and fauna.
+          </MainSectionSummary>
+        </MainSection>
+        <MainSection>
+          <MainSectionHeader>Mission</MainSectionHeader>
+          <MainSectionSummary>
+            We are interested in continuous massive tree planting events to help
+            sequester carbon footprint and stablize climate change extremes.
+          </MainSectionSummary>
+        </MainSection>
+        <MainSection>
+          <MainSectionHeader>Who we are!</MainSectionHeader>
+          <MainSectionSummary>
+            Water the Trees begun as a tree tracking platform to track the life
+            and health of a tree worldwide.
+          </MainSectionSummary>
+          <MainSectionSummary>
+            Together with data, three enthusiast, and volunteers we are able to
+            maintain a source to see the growth of trees in all communities.
+          </MainSectionSummary>
+        </MainSection>
+
+        <MainSection>
+          <MainSectionHeader>Join the team!</MainSectionHeader>
+          <MainSectionSummary>
             We are always looking for team members to contribute to the project.
-          </p>
-          <div className="about__main__section__subsection">
-            <p>
-              Currently we are looking for Backend Developers,Frontend
-              Developers,
-            </p>
-            <p>Designers, Researchers, Admin, Marketing and many more!</p>
-          </div>
-          <div className="about__main__section__subsection2">
-            <h3 className="about__main__section__subsection2__header">
-              How to get started:
-            </h3>
-            <p>
+          </MainSectionSummary>
+          <MainSectionSummary>
+            Currently we are looking for Backend Developers,Frontend Developers,
+            Designers, Researchers, Admin, Marketing and many more!
+          </MainSectionSummary>
+          <MainSubSection>
+            <MainSubSectionHeader>How to get started:</MainSubSectionHeader>
+            <MainSectionSummary>
               1. Read our{' '}
               <a
                 target="_blank"
@@ -84,8 +170,8 @@ const About = () => {
               >
                 Onboarding Steps
               </a>
-            </p>
-            <p>
+            </MainSectionSummary>
+            <MainSectionSummary>
               2. Join us on{' '}
               <a
                 target="_blank"
@@ -94,25 +180,23 @@ const About = () => {
               >
                 Slack
               </a>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="about__footer">
-        <div className="about__footer__section">
-          <h2>Community</h2>
-          <span>
-            To save trees in your local community, please visit our{' '}
-            <Link to="/community">Community</Link> outreach page to find
-            organizations near you.
-          </span>
-          <div className="about__footer__section__affiliates">
-            <Affiliates />
-          </div>
-        </div>
-      </div>
+            </MainSectionSummary>
+          </MainSubSection>
+        </MainSection>
+      </MainContainer>
+      <FooterContainer>
+        <MainSectionHeader>Community</MainSectionHeader>
+        <MainSectionSummary>
+          To save trees in your local community, please visit our{' '}
+          <Link to="/community">Community</Link> outreach page to find
+          organizations near you.
+        </MainSectionSummary>
+        <AffiliatesContainer>
+          <Affiliates />
+        </AffiliatesContainer>
+      </FooterContainer>
       <Footer />
-    </div>
+    </PageContainer>
   );
 };
 
