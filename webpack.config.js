dcf8baeff8c0a9acaaa0c9f56f784414babb3bd6
Zainab Tijani <jani.tijj@gmail.com>
2022-12-11 22:44:10 +0100

    implement holiday favicon to site (#371)

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ module.exports = (env) => {
     plugins: removeEmpty([
       new HtmlWebPackPlugin({
         favicon:
-          './client/src/assets/images/favicons/wtt-halloween-favicon.png',
+          './client/src/assets/images/favicons/wtt-christmas-favicon.png',
         template: './client/src/index.html',
         filename: './index.html',
         minify: false,
