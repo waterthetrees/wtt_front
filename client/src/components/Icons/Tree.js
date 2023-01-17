aa659327ba000066101400c2914c695d6be4a58d
Vern Fong Chao <91238232+vernfongchao@users.noreply.github.com>
2023-01-17 13:10:34 -0800

    Feature:-Footer Redesign #367 (#405)
    * add tree logo
    
    change font to Montserrat
    
    remove position and z-index
    
    * correct BEM styling for classes

---
+++ client/src/components/Icons/Tree.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TreeImage from '@/assets/images/addtree/treefattrunk.png';
+import { CardMedia } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const TreeLogo = () => {
+  const StyledLogo = styled(CardMedia)({
+    width: '32px',
+    aspectRatio: '1/1',
+  });
+
+  return <StyledLogo component="img" src={TreeImage} alt="tree"></StyledLogo>;
+};
