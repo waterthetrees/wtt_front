3878376d3142ddcfd8f3c5935b0b82885d45f399
vernfongchao <Vernchao93@gmail.com>
2023-01-19 13:28:32 -0800

    refractored from style components to css
    removed unused styled components

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,206 +1,166 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { Affiliates } from '@/components/Contact/Index';
 import { Footer } from '@/components/Footer/Footer';
 import tree from '../../assets/images/abouttree.jpg';
-import * as Page from '@/components/Section/Page';
+import { GreenButton } from '@/components/Section/Page';
 
 import './About.scss';
 
 const About = () => {
-  const NumberContainer = styled.div({
-    position: 'relative',
-    width: '100%',
-    bottom: '4rem',
-  });
-
-  const NumberContainer2 = styled.div({
-    position: 'absolute',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    color: '#3fab4530',
-  });
-
-  const Number = styled.h1({
-    fontSize: '15.5rem',
-  });
-
-  const AffiliatesContainer = styled.div({
-    width: '50%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginTop: '32px',
-    marginBottom: '24px',
-    img: {
-      height: '64px',
-    },
-    '@media(max-width: 768px)': {
-      width: '100%',
-    },
-  });
-
   return (
-    <Page.PageContainer>
-      <Page.Main>
-        <Page.MainSections>
-          <Page.SubSection>
-            <Page.SubSectionHeader>
+    <div className="about">
+      <div className="about__main">
+        <div className="about__main__section">
+          <div className="about__main__section__sub">
+            <h1>
               We are connecting our forest{' '}
-              <Page.GreenText>to our digital world</Page.GreenText>
-            </Page.SubSectionHeader>
-          </Page.SubSection>
-          <Page.SubSection>
-            <Page.SubSectionHeader2>About Us</Page.SubSectionHeader2>
-            <Page.SubSectionText>
+              <span className="about__main__section__green">
+                to our digital world
+              </span>
+            </h1>
+          </div>
+          <div className="about__main__section__sub">
+            <h2>About Us</h2>
+            <p>
               Welcome to Water the Trees, a platform that crowd sources tree
               planting and maintenance. We are an open source project run by
               tree planting volunteers.
-            </Page.SubSectionText>
-          </Page.SubSection>
-        </Page.MainSections>
-        <Page.ImageSection>
-          <Page.Image src={tree}></Page.Image>
-        </Page.ImageSection>
-        <Page.MainSections>
-          <Page.SubSection>
-            <Page.SubSectionHeader>
-              Together we can <Page.GreenText>map out the world</Page.GreenText>
-            </Page.SubSectionHeader>
-          </Page.SubSection>
-          <Page.SubSection>
-            <Page.SubSectionHeader2>Vision</Page.SubSectionHeader2>
-            <Page.SubSectionText>
+            </p>
+          </div>
+        </div>
+        <div className="about__main__section__image">
+          <img src={tree} alt="tree" />
+        </div>
+        <div className="about__main__section">
+          <div className="about__main__section__sub">
+            <h1>
+              Together we can{' '}
+              <span className="about__main__section__green">
+                map out the world
+              </span>
+            </h1>
+          </div>
+          <div className="about__main__section__sub">
+            <h2>Vision</h2>
+            <p>
               Crowdsource the planting and maintenance of our world's tree to
               quickly mitigate climate change.
-            </Page.SubSectionText>
-            <Page.SubSectionHeader2>Mission</Page.SubSectionHeader2>
-            <Page.SubSectionText>
+            </p>
+            <h2>Mission</h2>
+            <p>
               Trees provide the very necessities of life itself. They provide
               oxygen for us to breath, clean our air, protect our drinking
               water, create healthy communities, mitigate global warming, and
               can help stablilize the occuring drastic climate change,
               WaterTheTrees is dedicated to crowdsourcing the planting and
               maintenance of our world's trees.
-            </Page.SubSectionText>
-            <Page.SubSectionHeader2>Strategies & Goals</Page.SubSectionHeader2>
-            <Page.SubSectionText>
-              Keep a record of tree health and maintenance.
-            </Page.SubSectionText>
-            <Page.SubSectionText>
+            </p>
+            <h2>Strategies & Goals</h2>
+            <p>Keep a record of tree health and maintenance.</p>
+            <p>
               Enable people to adopt and follow neightborhood and favorite
               trees.
-            </Page.SubSectionText>
-            <Page.SubSectionText>
-              Notify people when trees need watering.
-            </Page.SubSectionText>
-            <Page.SubSectionText>
-              Allow cities and tree organizations to make and track trees.
-            </Page.SubSectionText>
-          </Page.SubSection>
-        </Page.MainSections>
-        <Page.MainSections>
-          <Page.NumberSubSection>
-            <NumberContainer>
-              <NumberContainer2>
-                <Number>1</Number>
-              </NumberContainer2>
-            </NumberContainer>
-            <Page.SubSectionHeader2>Plant More Trees</Page.SubSectionHeader2>
-            <Page.SubSectionText>
+            </p>
+            <p>Notify people when trees need watering.</p>
+            <p>Allow cities and tree organizations to make and track trees.</p>
+          </div>
+        </div>
+        <div className="about__main__section">
+          <div className="about__main__section__numsection">
+            <div className="about__main__section__numsection__relative">
+              <div className="about__main__section__numsection__absolute">
+                <span>1</span>
+              </div>
+            </div>
+            <h2>Plant More Trees</h2>
+            <p>
               Our goal, we want everyone to plant more trees to provide
               ourselves a better future to control climate change.
-            </Page.SubSectionText>
-          </Page.NumberSubSection>
-          <Page.NumberSubSection>
-            <NumberContainer>
-              <NumberContainer2>
-                <Number>2</Number>
-              </NumberContainer2>
-            </NumberContainer>
-            <Page.SubSectionHeader2>
-              Keep More Trees Alive
-            </Page.SubSectionHeader2>
-            <Page.SubSectionText>
+            </p>
+          </div>
+          <div className="about__main__section__numsection">
+            <div className="about__main__section__numsection__relative">
+              <div className="about__main__section__numsection__absolute">
+                <span>2</span>
+              </div>
+            </div>
+            <h2>Keep More Trees Alive</h2>
+            <p>
               As the climate continues to heat up trees suffer. We want to
               maintain as many trees as possible worldwide.
-            </Page.SubSectionText>
-          </Page.NumberSubSection>
-          <Page.NumberSubSection>
-            <NumberContainer>
-              <NumberContainer2>
-                <Number>3</Number>
-              </NumberContainer2>
-            </NumberContainer>
-            <Page.SubSectionHeader2>
-              Involve the general public
-            </Page.SubSectionHeader2>
-            <Page.SubSectionText>
+            </p>
+          </div>
+          <div className="about__main__section__numsection">
+            <div className="about__main__section__numsection__relative">
+              <div className="about__main__section__numsection__absolute">
+                <span>3</span>
+              </div>
+            </div>
+            <h2>Involve the general public</h2>
+            <p>
               To grow our tree population, we want everyone to contribute to
               local tree planting and be aware of tree health.
-            </Page.SubSectionText>
-          </Page.NumberSubSection>
-        </Page.MainSections>
-        <Page.MainSections>
-          <Page.SubSection>
-            <Page.SubSectionHeader>
-              <Page.GreenText>Join Our Team!</Page.GreenText>
-            </Page.SubSectionHeader>
-          </Page.SubSection>
-          <Page.SubSection>
-            <Page.SubSectionHeader2>
-              Everyone is Welcomed!
-            </Page.SubSectionHeader2>
-            <Page.SubSectionText>
+            </p>
+          </div>
+        </div>
+        <div className="about__main__section">
+          <div className="about__main__section__sub">
+            <h1>
+              <span className="about__main__section__green">
+                Join Our Team!
+              </span>
+            </h1>
+          </div>
+          <div className="about__main__section__sub">
+            <h2>Everyone is Welcomed!</h2>
+            <p>
               We are always looking for team members to contribute to the
               project. Currently we are looking for Backend Developers, Frontend
               Developers, UX/UI Designers, Researchers, Admin, Marketing.
-            </Page.SubSectionText>
-            <Page.SubSectionHeader2>Get started by:</Page.SubSectionHeader2>
-            <Page.SubSectionText>
+            </p>
+            <h2>Get started by:</h2>
+            <p>
               1. Read our{' '}
-              <Page.WebLink
+              <a
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://docs.google.com/document/d/1L5Hc8_K_NhVhAejdE05C_Y__CgqeBWFrFYFoNqBSBbQ/edit"
               >
                 Onboarding Steps
-              </Page.WebLink>
+              </a>
               .
-            </Page.SubSectionText>
-            <Page.SubSectionText>
+            </p>
+            <p>
               2. Join us on{' '}
-              <Page.WebLink
+              <a
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://sfbrigade.slack.com/archives/C010EGACUTU"
               >
                 Slack
-              </Page.WebLink>
+              </a>
               , and say Hi!
-            </Page.SubSectionText>
+            </p>
             <a
               target="_blank"
               rel="noopener noreferrer"
               href="https://sfbrigade.slack.com/archives/C010EGACUTU"
             >
-              <Page.GreenButton>Join our Slack</Page.GreenButton>
+              <GreenButton>Join our Slack</GreenButton>
             </a>
-          </Page.SubSection>
-        </Page.MainSections>
-        <Page.MainSections>
-          <Page.AffiliatesSubSection>
-            <Page.SubSectionText>
-              A few organizations we'd like to thank for their support.
-            </Page.SubSectionText>
-            <AffiliatesContainer>
+          </div>
+        </div>
+        <div className="about__main__section">
+          <div className="about__main__section__affiliate">
+            <p>A few organizations we'd like to thank for their support.</p>
+            <div className="about__main__section__affiliate__container">
               <Affiliates />
-            </AffiliatesContainer>
-          </Page.AffiliatesSubSection>
-        </Page.MainSections>
-      </Page.Main>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
-    </Page.PageContainer>
+    </div>
   );
 };
 
