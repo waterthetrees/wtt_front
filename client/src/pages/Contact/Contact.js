8d774af4f1b6db0763fabcfbfe9e34de7e46112b
vernfongchao <Vernchao93@gmail.com>
2023-01-19 20:20:11 -0800

    add Icons and logos
    refractor contact image section added

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,69 +1,36 @@
 import React from 'react';
 import { Footer } from '@/components/Footer/Footer';
-import CodeLogo from '@/assets/images/logos/c4sf-logo.jpg';
-import SierrClubLogo from '@/assets/images/logos/san-francisco-bay.png';
-import Contacts from './Contacts';
+import { C4SF } from '@/components/Contacts';
+import { SlackLogo, GithubLogo, FigmaLogo } from '@/components/Icons';
+import tree from '../../assets/images/addtree/tree.png';
+import { GreenButton } from '@/components/Button/Green';
+
 import './Contact.scss';
 
 export default function Contact() {
   return (
     <div className="contact">
-      <div className="contact__header">
-        <h2>Ways to get in touch:</h2>
-      </div>
-
       <div className="contact__main">
-        <div className="contact__p">
-          <span>
-            Join us on slack to get involved in coding for the trees and climate
-            change!
-          </span>
-          <Contacts />
-          <span>
-            Or, email us at{' '}
-            <a
-              href="mailto:info@waterthetrees.com"
-              rel="noreferrer"
-              target="_blank"
-            >
-              info@waterthetrees.com
-            </a>
-          </span>
+        <div className="contact__main__image">
+          <img className="contact__main__image__img" src={tree} alt="tree" />
+          <div className="contact__main__image__absolute">
+            <div className="contact__main__image__relative">
+              <h1>Get in Touch with us!</h1>
+              <h2>We meet every week on wednesday at 6:30 PM PST.</h2>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://sfbrigade.slack.com/archives/C010EGACUTU"
+              >
+                <GreenButton
+                  styles={{ padding: '4px 12px', fontWeight: 'bold' }}
+                >
+                  Join our Slack
+                </GreenButton>
+              </a>
+            </div>
+          </div>
         </div>
-
-        <div className="contact__p">
-          <span>
-            Additionally, our parent organizations are always looking for
-            volunteers!
-          </span>
-          <span>For developers:</span>
-          <a
-            href="https://www.codeforsanfrancisco.org/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img
-              alt="Code for America Logo"
-              src={CodeLogo}
-              className="contact__logo"
-            />
-            Code for America San Francisco
-          </a>
-          <span>For tree afficionados and climate enthusiasts:</span>
-          <a
-            href="https://www.sierraclub.org/san-francisco-bay/tree-planting"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img
-              alt="Sierra Club Logo"
-              src={SierrClubLogo}
-              className="contact__logo"
-            />
-            Sierra Club Tree Team
-          </a>
-        </div>
-        <Footer />
       </div>
     </div>
   );
