fd741556dda9c5d96d9b1cbc15a11083549e46fb
Paul Grad <paulgrad3rd@gmail.com>
2023-03-30 16:23:32 -0700

    test: Add Lighthouse CI locally
    Added the Lighthouse CI config file.
    I had to tweak the info message
    server/index.js emits, because Lighthouse
    needs to see something like "Listening on..."
    to detect that the server has started.

---
+++ lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npm run start:dev',
+      url: ['http://localhost:3001'],
+    },
+    assert: {
+      assertions: {
+        'categories:accessibility': ['error', { minScore: 1 }],
+        'categories:best-practices': ['error', { minScore: 1 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
