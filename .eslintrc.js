02d1a103d8515c040c169960ae5a384c1c8467b4
Paul Grad <paulgrad3rd@gmail.com>
2023-04-04 13:14:21 -0700

    test: Add Storybook
    To speed up dev cycles and allow
    for testing of individual components.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,34 +4,27 @@ module.exports = {
     es2022: true,
     browser: true,
     node: true,
-    jest: true,
+    jest: true
   },
-  extends: [
-    'eslint:recommended', // https://eslint.org/docs/rules/
-    'plugin:import/recommended', // https://github.com/import-js/eslint-plugin-import
-    'plugin:jsx-a11y/recommended', // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y
-    'plugin:prettier/recommended', // https://github.com/prettier/eslint-plugin-prettier
-    'plugin:react/recommended', // https://github.com/yannickcr/eslint-plugin-react
-    'plugin:react-hooks/recommended', // https://www.npmjs.com/package/eslint-plugin-react-hooks
-  ],
+  extends: ['eslint:recommended', 'plugin:import/recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
-      impliedStrict: true,
-    },
+      impliedStrict: true
+    }
   },
   settings: {
     react: {
-      version: 'detect',
+      version: 'detect'
     },
     'import/resolver': {
-      alias: [['@', './client/src']],
-    },
+      alias: [['@', './client/src']]
+    }
   },
   rules: {
     'react/prop-types': 'off',
-    'no-console': 'warn',
-  },
+    'no-console': 'warn'
+  }
 };
