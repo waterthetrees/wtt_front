1ed90e2e01e8394feb83772757db329ad27707b1
vernfongchao <vernchao93@gmail.com>
2023-04-04 16:15:09 -0700

    added content to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,28 @@ import { TreeLogo } from '@/components/Icons';
 
 const Footer = () => (
   <div className="footer">
+    <div className="footer__follow">
+      <div className="footer__section">
+        <span>Follow Us</span>
+      </div>
+    </div>
     <div className="footer__content">
+      <div className="footer__section1">
+        Directory
+        <span>Map</span>
+        <span>About Us</span>
+        <span>Contact Us</span>
+        <span>Donate</span>
+      </div>
+      <div className="footer__section1">
+        Tree Data
+        <span>Source</span>
+        <span>Data</span>
+        <span>Taxonomy</span>
+      </div>
+      <div className="footer__section2"> Collaborate</div>
+    </div>
+    <div className="footer__info">
       <Link to="/">
         <TreeLogo />
       </Link>
