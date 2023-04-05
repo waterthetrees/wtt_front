86ff4ed3f27f02d0b535cf6f941ec51179a9e41c
Paul Grad <paulgrad3rd@gmail.com>
2023-04-05 15:44:10 -0700

    test: Add Tree story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,15 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
+import webpackConfigFunc from '../webpack.config';
+
 const config = {
+  webpackFinal: (config) => {
+    const webpackConfig = webpackConfigFunc("analyze");
+    config.resolve.alias = {
+      ...webpackConfig.resolve.alias,
+      ...config.resolve.alias
+    }
+    return config;
+  },
   stories: ['../stories/**/*.mdx', '../client/src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
