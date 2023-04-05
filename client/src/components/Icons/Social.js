9dc036dc26f87493b338c31a904654b0d47a7257
vernfongchao <vernchao93@gmail.com>
2023-04-04 17:06:22 -0700

    add social icons

---
+++ client/src/components/Icons/Social.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { CardMedia } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const SlackLogo = () => {
+  const StyledLogo = styled(CardMedia)({
+    height: '64px',
+    aspectRatio: '1/1',
+  });
+
+  return (
+    <StyledLogo
+      component="img"
+      src="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2019/01/Slack-New.jpg"
+      alt="tree"
+    ></StyledLogo>
+  );
+};
+
+export const GithubLogo = () => {
+  const StyledLogo = styled(CardMedia)({
+    height: '64px',
+    aspectRatio: '1/1',
+  });
+
+  return (
+    <StyledLogo
+      component="img"
+      src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+      alt="tree"
+    ></StyledLogo>
+  );
+};
+
+export const FigmaLogo = () => {
+  const StyledLogo = styled(CardMedia)({
+    height: '64px',
+    aspectRatio: '1/1',
+    objectFit: 'fill',
+  });
+
+  return (
+    <StyledLogo
+      component="img"
+      src="https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg"
+      alt="tree"
+    ></StyledLogo>
+  );
+};
+
+export const LinkedinLogo = () => {
+  const StyledLogo = styled(CardMedia)({});
+  return (
+    <StyledLogo
+      component="img"
+      src="https://www.citypng.com/public/uploads/preview/hd-linkedin-square-blue-icon-transparent-png-11640440454pa2qjzikf1.png"
+      alt="linkedin"
+    ></StyledLogo>
+  );
+};
