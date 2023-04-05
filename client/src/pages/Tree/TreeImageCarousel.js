5d9ac1218b957a35f10da4386eb537240aec079c
Paul Grad <paulgrad3rd@gmail.com>
2023-04-05 14:33:18 -0700

    feat: Add new carousel story
    Developed a new carousel with
    react-spring-carousel. Given
    that we don't have the image
    upload implemented yet, it's a
    lot easier to play around with
    the carousel in storybook
    then in the app.

--- client/src/pages/Tree/TreeImageCarousel.js
+++ client/src/pages/Tree/TreeImageCarousel.js
@@ -1,25 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSpringCarousel } from "react-spring-carousel";
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import { IconButton } from "@mui/material";
+import { ArrowBack, ArrowForward, Circle, CircleOutlined } from "@mui/icons-material";
 
 export default function TreeImageCarousel({ imgs }) {
+  const maxWidth = Math.max(...imgs.map(img => img.width));
+  const maxHeight = Math.max(...imgs.map(img => img.height));
+
+  const [activeItem, setActiveItem] = useState("item-0");
+
   const {
+    useListenToCustomEvent,
     carouselFragment,
     slideToPrevItem,
     slideToNextItem,
   } = useSpringCarousel({
-    items: (imgs.map((img, idx) =>
-      ({
-        id: `img-item-${idx}`,
-        renderItem: <img src={img.src} alt={img.txt} />
-      })
-    ))
+    withLoop: true,
+    gutter: 50,
+    items: (
+      imgs.map((img, i) => ({
+        id: `item-${i}`,
+        renderItem:
+          <div style={{
+            display: "flex",
+            backgroundColor: "rgba(0, 0, 0, .1)",
+            alignItems: "center",
+            width: `${maxWidth}px`,
+            height: `${maxHeight}px`,
+          }}>
+            <img
+              key={i}
+              src={img.src}
+              style={{ display: "inline-block" }}
+              alt={img.text}
+            />
+          </div>
+      }))
+    )
   });
 
+  useListenToCustomEvent((event) => {
+    if (event.eventName === "onSlideStartChange") {
+      setActiveItem(event.nextItem.id)
+    } 
+  });
+
+  const indicators = [];
+  for (let i = 0; i < imgs.length; ++i) {
+    const itemName = `item-${i}`;
+    if (itemName === activeItem) {
+      indicators.push(<Circle key={i} />);
+    } else {
+      indicators.push(<CircleOutlined key={i} />);
+    }
+  }
+
   return (
-    <div>
-      <button onClick={slideToPrevItem}>Prev Item</button>
-      <div>{carouselFragment}</div>
-      <button onClick={slideToNextItem}>Next Item</button>
+    <div style={{ 
+      display: "flex",
+      flexDirection: "column",
+      width: `${maxWidth}px`,
+      margin: "10px",
+    }}>
+      <IconButton
+        onClick={slideToPrevItem}
+        sx={{ 
+          alignSelf: "flex-start",
+          position: "absolute",
+          top: `${maxHeight / 2}px`,
+          zIndex: 1,
+          backgroundColor: "rgba(0, 128, 0, .8)",
+          color: "white",
+          '&:hover': {
+            backgroundColor: "white",
+            color: "green",
+          }
+        }}
+        fontSize="large"
+        color="white"
+      >
+        <ArrowBack
+          sx={{ fontSize: "2em" }}
+        />
+      </IconButton>
+      <div style={{ zIndex: 0 }}>
+        {carouselFragment}
+      </div>
+      <IconButton
+        onClick={slideToNextItem}
+        sx={{
+          alignSelf: "flex-end",
+          position: "absolute",
+          top: `${maxHeight / 2}px`,
+          zIndex: 1,
+          backgroundColor: "rgba(0, 128, 0, .8)",
+          color: "white",
+          '&:hover': {
+            backgroundColor: "white",
+            color: "green",
+          }
+        }}
+        fontSize="large"
+      >
+        <ArrowForward
+          sx={{ fontSize: "2em" }}
+        />
+      </IconButton>
+      <div style={{ display: "flex", alignSelf: "center", justifyContent: "center", margin: "1em", gap: ".3em", color: "green" }} >
+        {indicators}
+      </div>
     </div>
   );
 }
