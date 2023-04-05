56d291154791232445ec1440e2531ecb80386415
Paul Grad <paulgrad3rd@gmail.com>
2023-04-05 15:44:10 -0700

    test: Add Tree story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,16 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
+import webpackConfigFunc from '../webpack.config';
+
 const config = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  webpackFinal: (config) => {
+    const webpackConfig = webpackConfigFunc("analyze");
+    config.resolve.alias = {
+      ...webpackConfig.resolve.alias,
+      ...config.resolve.alias
+    }
+    return config;
+  },
+  stories: ['../stories/**/*.mdx', '../client/src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,24 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create a client for react-query calls.  Don't automatically refetch the data when the window is
+// focused since it's not changing that frequently.
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+});
+
+// Create an MUI theme so that any child can access it for styling via the hooks.
+const theme = createTheme({
+  typography: {
+    fontFamily: 'inherit',
+  },
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +28,12 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (story) =>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        {story()}
+      </QueryClientProvider>
+    </ThemeProvider>
+];
