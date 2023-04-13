33c071a6712601256f3433b7c6e30654b9f06ce0
zoobot <rommims@gmail.com>
2023-04-13 10:55:20 -0700

    Treelist Epic: Toggle to switch between list and card views

---
+++ client/src/components/Form/Toggle.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ViewList, ViewModule } from '@mui/icons-material';
+import { ToggleButtonGroup, ToggleButton } from '@mui/material';
+
+export default function Toggle({ view, setView }) {
+  const handleToggleView = (event, newView) => {
+    // to persist view on refresh set in local storage
+    // before setView triggers a re-render setting state
+    localStorage.setItem('view', newView);
+    setView(newView);
+  };
+  return (
+    <ToggleButtonGroup
+      orientation="horizontal"
+      value={localStorage.getItem('view') || view}
+      exclusive={true}
+      onChange={handleToggleView}
+      color="success"
+      sx={{ backgroundColor: 'white' }}
+    >
+      <ToggleButton value="list" aria-label="list">
+        <ViewList />
+      </ToggleButton>
+      <ToggleButton value="card" aria-label="card">
+        <ViewModule />
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
