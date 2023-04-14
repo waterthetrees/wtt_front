6082a269152935d58e74e96b0251bc10bb13d5b6
zoobot <rommims@gmail.com>
2023-04-13 19:13:19 -0700

    Treelist Epic: Added component ButtonShowHide with assets arrow-hide, arrow-show, and test ButtonShowHide.test

--- client/src/components/Button/ButtonShowHide.js
+++ client/src/components/Button/ButtonShowHide.js
@@ -5,17 +5,17 @@ const treeImagesPath = 'assets/images/trees/';
 
 export const ButtonShowHide = ({ showMore, setShowMore }) => {
   const onClick = () => setShowMore(!showMore);
-  const arrowDirection = showMore
-    ? `${treeImagesPath}arrowup.svg`
-    : `${treeImagesPath}arrowdown.svg`;
+  const arrowDirection = showMore ? `hide` : `show`;
+  const pathName = `${treeImagesPath}arrow-${arrowDirection}.svg`;
   return (
     <button
-      aria-label="showHide"
+      aria-label={`Button-${arrowDirection}`}
       type="button"
       onClick={onClick}
       className="showhide"
+      title={arrowDirection}
     >
-      <img alt="showhide" className="showhide__img" src={arrowDirection} />
+      <img alt={arrowDirection} className="showhide__img" src={pathName} />
     </button>
   );
 };
