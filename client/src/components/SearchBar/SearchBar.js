64516263f4c427b076c58637368ce131d2ee7f98
zoobot <rommims@gmail.com>
2023-04-17 13:32:05 -0700

    Update snaps

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,13 @@
 import React from 'react';
-
 import { Search } from '@/components/Icons';
-
 import './SearchBar.scss';
+
 export const SearchBar = (props) => {
   return (
     <div className="searchbar" style={{ ...props?.style?.div }}>
       <label htmlFor="searchbar">
         <Search
           sx={{
-            color: '#00000050',
             fontSize: '24px',
             marginLeft: '10px',
             marginRight: '10px',
@@ -20,7 +18,7 @@ export const SearchBar = (props) => {
 
       <input
         id="searchbar"
-        style={{ ...props.style.input }}
+        style={{ ...props?.style?.input }}
         value={props.search}
         onChange={props.onChange}
         placeholder={props.placeholder}
