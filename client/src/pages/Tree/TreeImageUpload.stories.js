20a41b59aa0e857517dc976a947051cdb84e6945
Paul Grad <paulgrad3rd@gmail.com>
2023-04-18 16:27:30 -0700

    test: unit tests

--- client/src/pages/Tree/TreeImageUpload.stories.js
+++ client/src/pages/Tree/TreeImageUpload.stories.js
@@ -28,7 +28,6 @@ export const Default = {
     apiUploadDone: () => { console.log("upload done"); }, 
     apiCancelUpload: () => { console.log("upload cancelled"); }, 
     apiPollUpload: pollUpload,
-    apiFileDialog: () => "random",
     isMobile: false
   },
 }
