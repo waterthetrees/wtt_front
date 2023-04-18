315b22287f57ad560ae375bd695d92ff0cce67ab
zhiyanz <zhiyanczhong@gmail.com>
2023-04-18 03:43:57 -0400

    Issues 476 and 480: 404 page redesign

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,49 +1,34 @@
 import React from 'react';
-import { Grid, Typography, Button, Container } from '@mui/material';
-import { Footer } from '@/components/Footer/Footer';
 import './NotFound.scss';
 import { Link } from 'react-router-dom';
+import ErrorImage from '../../assets/images/404image.svg';
 
 export default function NotFound() {
   return (
     <div className="not-found">
-      <div className="background-overlay"></div>
-      <Container>
-        <Grid
-          container
-          justifyContent={{ md: 'center', sm: 'flex-start' }}
-          alignItems="flex-start"
-          direction="column"
-          sx={{ height: '100vh' }}
-        >
-          <Grid item>
-            <Typography variant="h1">404</Typography>
-          </Grid>
-          <Grid item sx={{ mb: 5 }}>
-            <Typography paragraph>
-              Sorry, we can&apos;t find that page! Don&apos;t worry though,
-              there are still plenty of trees to water!
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Button
-              variant="outlined"
-              size="large"
-              sx={{
-                color: 'white',
-                backgroundColor: 'black',
-                border: '2px solid white',
-                fontSize: '1.5rem',
-                fontWeight: '800',
-              }}
-            >
-              <Link to="/">water the trees</Link>
-            </Button>
-          </Grid>
-          <Footer />
-        </Grid>
-        <Footer />
-      </Container>
+      <div className="content-container">
+        <div className="error-message">
+          <div className="image-container">
+            <div className="alert-text">404</div>
+            <img src={ErrorImage} alt="404" />
+          </div>
+
+          <div className="error-text">
+            <p className="text_top">Aww, nuts.</p>
+            <p className="text_bottom">
+              Looks like you got off track. <br />
+              Don’t worry though, there are <br />
+              plenty of trees to water!
+            </p>
+          </div>
+        </div>
+
+        <button type="button" className="home-button">
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            Go to Map
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
