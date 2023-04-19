e3869f50e4d4c7ec1c02eba8dc5e784c0a8a3a5a
zhiyanz <zhiyanczhong@gmail.com>
2023-04-18 22:34:04 -0400

    updated class names

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -5,17 +5,17 @@ import ErrorImage from '../../assets/images/404image.svg';
 
 export default function NotFound() {
   return (
-    <div className="not-found">
+    <div className="not-found-page">
       <div className="content-container">
-        <div className="error-message">
+        <div className="error-messages">
           <div className="image-container">
             <div className="alert-text">404</div>
             <img src={ErrorImage} alt="404" />
           </div>
 
           <div className="error-text">
-            <p className="text_top">Aww, nuts.</p>
-            <p className="text_bottom">
+            <p className="error-text-top">Aww, nuts.</p>
+            <p className="error-text-bottom">
               Looks like you got off track. <br />
               Don’t worry though, there are <br />
               plenty of trees to water!
