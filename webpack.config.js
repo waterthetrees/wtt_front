da3a3553f00333815583801ca5fe451fdab55231
Paul Grad <paulgrad3rd@gmail.com>
2023-04-25 20:31:06 -0700

    fix: only pull in resolve part of config

--- webpack.config.js
+++ webpack.config.js
@@ -149,7 +149,7 @@ module.exports = (env) => {
   // modifies the original object, so pass it a copy of config so we keep the
   // unmodified original.
   return ifAnalyze(
-    new SpeedMeasurePlugin().wrap(config),
+    new SpeedMeasurePlugin().wrap({ plugins: config.plugins, resolve: config.resolve }),
     config,
   );
 };
