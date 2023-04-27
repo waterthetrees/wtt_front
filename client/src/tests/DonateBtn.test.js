67f5b208f0a24f4af0360bfa7063f14d2bd2a7dc
Zainab <jani.tijj@gmail.com>
2023-04-28 00:48:32 +0100

    Test: added test for Donate component

---
+++ client/src/tests/DonateBtn.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+// import userEvent from '@testing-library/user-event';
+import DonateBtn from '@/components/Button/DonateBtn/DonateBtn';
+
+describe('DonateBtn', () => {
+  test('renders the Donate button', () => {
+    render(<DonateBtn />);
+    const donateButton = screen.getByText('Donate');
+    expect(donateButton).toBeInTheDocument();
+  });
+
+  test('has the correct URL', () => {
+    render(<DonateBtn />);
+    const donateLink = screen.getByRole('link', { name: /donate/i });
+    expect(donateLink).toHaveAttribute(
+      'href',
+      'https://opencollective.com/waterthetrees/donate',
+    );
+  });
+
+  test('opens the URL in a new tab', () => {
+    render(<DonateBtn />);
+    const donateLink = screen.getByRole('link', { name: /donate/i });
+    expect(donateLink).toHaveAttribute('target', '_blank');
+    expect(donateLink).toHaveAttribute('rel', 'noreferrer');
+  });
+});
