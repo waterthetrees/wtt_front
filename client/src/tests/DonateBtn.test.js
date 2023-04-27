1855fc44c370b9c483b4d0989554427f9853e1f5
Zainab <jani.tijj@gmail.com>
2023-04-28 00:58:43 +0100

    clear: remove comments

--- client/src/tests/DonateBtn.test.js
+++ client/src/tests/DonateBtn.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
-// import userEvent from '@testing-library/user-event';
 import DonateBtn from '@/components/Button/DonateBtn/DonateBtn';
 
 describe('DonateBtn', () => {
