87d1ef2e4f3493235faf3dda0acfaf993ed0d45a
Mason Park <mwp2010@gmail.com>
2023-04-29 14:33:38 -0700

    Separate out getting zoom level from setCenter

--- client/src/pages/Map/MapboxManager.js
+++ client/src/pages/Map/MapboxManager.js
@@ -1,7 +1,10 @@
 import { REGION_TYPES } from '../../util/constants';
 
 class MapboxManager {
-  // map is expected to be nullable. Mostly serves as a way to pass in a mock in tests
+  // @param {mapboxgl.Map} markerRef: a mapboxgl.Map() object
+  // See https://docs.mapbox.com/mapbox-gl-js/api/map/
+  // map is expected to be nullable initially while map.js creates the mapboxgl.Map() object.
+  // Mostly serves as a way to pass in a mock in tests
   constructor(map) {
     this.map = map;
   }
@@ -31,49 +34,43 @@ class MapboxManager {
       regionType = regionType[0];
     }
 
-    switch (regionType) {
-      case REGION_TYPES.COUNTRY:
-        this.map.flyTo({
-          center: coords,
-          zoom: 4.5,
-        });
-        break;
-      case REGION_TYPES.REGION:
-        this.map.flyTo({
-          center: coords,
-          zoom: 6,
-        });
-        break;
-      case REGION_TYPES.PLACE:
-      case REGION_TYPES.DISTRICT:
-      case REGION_TYPES.POSTAL_CODE:
-        this.map.flyTo({
-          center: coords,
-          zoom: 12,
-        });
-        break;
-      case REGION_TYPES.NEIGHBORHOOD:
-      case REGION_TYPES.LOCALITY:
-        this.map.flyTo({
-          center: coords,
-          zoom: 14,
-        });
-        break;
-      case REGION_TYPES.ADDRESS:
-      case REGION_TYPES.LATLONG:
-      case REGION_TYPES.POI:
-        this.map.flyTo({
-          center: coords,
-          zoom: 17,
-        });
-        break;
-      default:
-        this.map.flyTo({
-          center: coords,
-        });
+    const zoomLevel = this._getZoom(regionType);
+    if (zoomLevel) {
+      this.map.flyTo({
+        center: coords,
+        zoom: zoomLevel,
+      });
+    } else {
+      this.map.flyTo({
+        center: coords,
+      });
     }
   }
 
+  _getZoom(regionType) {
+    switch (regionType) {
+      case REGION_TYPES.COUNTRY:
+        return 4.5;
+      case REGION_TYPES.REGION:
+        return 6;
+      case REGION_TYPES.PLACE:
+      case REGION_TYPES.DISTRICT:
+      case REGION_TYPES.POSTAL_CODE:
+        return 12;
+      case REGION_TYPES.NEIGHBORHOOD:
+      case REGION_TYPES.LOCALITY:
+        return 14;
+      case REGION_TYPES.ADDRESS:
+      case REGION_TYPES.LATLONG:
+      case REGION_TYPES.POI:
+        return 17;
+      default:
+        return null;
+    }
+  }
+
+  // @param {RefObject} markerRef: ref to a mapboxgl.Marker() object
+  // See https://docs.mapbox.com/mapbox-gl-js/api/markers/#marker#addto
   addMarkerRefToMap(markerRef) {
     if (!this.map) {
       return;
