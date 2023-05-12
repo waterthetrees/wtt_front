726f2e57383e26f057fbaca90447d1cb19455564
Paul Grad <117248915+PGrad@users.noreply.github.com>
2023-05-12 15:42:25 -0700

    feature: image upload (#664)
    * feat: add image upload html
    
    The basic html/css of the image
    upload button, without
    functionality. Following Randy's
    specs in the figma diagram.
    
    * feat: users can now drag and drop an image
    
    * feat: can upload from dnd or file dialog
    
    * feat: abstract out upload logic from component
    
    * feat: allow user to bring up file dialog
    
    * feat: separate desktop and mobile versions
    
    * feat: update button styling
    
    * feat: correctly handle file select dialog
    
    The upload area is now a button, and
    will bring up a file dialog when clicked.
    
    * chore: remove TreeImageUpload from Tree
    
    * test: jest and storybook addons
    
    Add Storybook addon for a11y,
    and jest plugins for testing.
    
    * fix: make sure upload area highlights on drag
    
    * feat: only allow jpg, allow submit on enter
    
    Only allow jpg files to be uploaded. Allow
    users to submit a file in the dialog by
    pressing enter.
    
    * test: unit tests
    
    * test: added unit tests for real this time
    
    * chore: remove deprecated storybook/testing-react
    
    * feat: can error on drag and drop or on dialog
    
    * test: fix unit test
    
    * fix: storybook/react is now a dev dep
    
    * feat(664): code review feedback
    
    Moved all the css styling into
    the .scss file. Set up Storybook
    so it could build .scss files.
    
    * Linted files
    
    * feat(667): use React.Fragment
    
    * test(667): update snapshots
    
    * fix(667): remove double import

---
+++ client/src/pages/Tree/TreeImageUpload.stories.js
@@ -0,0 +1,36 @@
+import TreeImageUpload from './TreeImageUpload';
+
+export default {
+  title: 'TreeImageUpload',
+  component: TreeImageUpload,
+};
+
+// A mock API for polling upload.
+async function pollUpload(progressHook, uploadDone) {
+  let progress = 0;
+  let intervalId = setInterval(() => {
+    progress = progress + 10;
+    progressHook(progress);
+    if (progress >= 100) {
+      clearInterval(intervalId);
+      intervalId = null;
+      uploadDone();
+    }
+  }, 300);
+}
+
+export const Default = {
+  args: {
+    apiUploadURL: (url) => {
+      console.log(`uploaded url: ${url}`);
+    },
+    apiUploadDone: () => {
+      console.log('upload done');
+    },
+    apiCancelUpload: () => {
+      console.log('upload cancelled');
+    },
+    apiPollUpload: pollUpload,
+    isMobile: false,
+  },
+};
