cd639df18a7316ad12547ec5d83f18c5516e07ed
John Lee <drumwolf@gmail.com>
2023-08-20 11:55:35 -0700

    User Profile Redesign - initial refactor (#741)
    * created stub for UserInfoContainer component
    
    * refactored UserInfoContainer completely
    
    * removed legacy code that has been refactored
    
    * moved UserInfoContainer out of 'components' folder and back into local 'Userprofile' folder

---
+++ client/src/pages/Userprofile/UserInfoContainer.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Avatar, Box, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Star } from '@mui/icons-material';
+import TreeIcon from '@/assets/images/addtree/tree12.svg';
+import AdoptionIcon from '@/components/Icons/AdoptionIcon/AdoptionIcon';
+import { TooltipBottom } from '@/components/Tooltip';
+
+const UserInfoContainerBox = styled(Box)`
+  display: inline-flex;
+  align-items: center;
+  margin-bottom: 2em;
+`;
+
+const UserAvatar = styled(Avatar)`
+  width: 8em;
+  height: 8em;
+  margin: 0 3em 0 2em;
+`;
+
+const iconStyle = {
+  height: '2em',
+  width: '2em',
+};
+
+const UserActionIcon = ({ title, icon, count }) => (
+  <TooltipBottom title={title}>
+    <div
+      style={{
+        marginRight: '1em',
+        display: 'inline-block',
+        fontSize: '1.25em',
+      }}
+    >
+      {icon}
+      <span style={{ marginLeft: '0.35rem' }}>{count}</span>
+    </div>
+  </TooltipBottom>
+);
+
+const UserIcons = ({ adoptedCount, likedCount, plantedCount }) => (
+  <Box sx={{ mb: 2 }}>
+    <UserActionIcon
+      title="Adopted"
+      count={adoptedCount}
+      icon={<AdoptionIcon style={iconStyle} />}
+    />
+    <UserActionIcon
+      title="Liked"
+      count={likedCount}
+      icon={<Star style={iconStyle} />}
+    />
+    <UserActionIcon
+      title="Planted"
+      count={plantedCount}
+      icon={
+        <img
+          alt=""
+          src={TreeIcon}
+          style={{ ...iconStyle, marginRight: '0.2em' }}
+        />
+      }
+    />
+  </Box>
+);
+
+const UserInfoContainer = ({ adoptedTrees, likedTrees, plantedTrees, user }) => {
+  const { name, nickname, email, picture } = user;
+  return (
+    <UserInfoContainerBox>
+      <UserAvatar alt="Avatar" src={picture} />
+      <div>
+        <UserIcons
+          adoptedCount={adoptedTrees.length}
+          likedCount={likedTrees.length}
+          plantedCount={plantedTrees.length}
+        />
+        <Typography variant="body1">{name}</Typography>
+        <Typography variant="body1">{nickname}</Typography>
+        <Typography variant="body1">{email}</Typography>
+      </div>
+    </UserInfoContainerBox>
+  )
+}
+export default UserInfoContainer
--- client/src/pages/Userprofile/UserProfile.js
+++ client/src/pages/Userprofile/UserProfile.js
@@ -1,85 +1,24 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Avatar, Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { Star } from '@mui/icons-material';
 import {
   useUserAdoptedQuery,
   useUserLikedQuery,
   useUserPlantedQuery,
   useUserTreeHistoryQuery,
 } from '@/api/queries';
-import AdoptionIcon from '@/components/Icons/AdoptionIcon/AdoptionIcon';
-import TreeIcon from '@/assets/images/addtree/tree12.svg';
+import UserInfoContainer from './UserInfoContainer';
 import UserTreeHistoryTable from '@/components/UserTreeHistoryTable/UserTreeHistoryTable';
 import { Footer } from '@/components/Footer/Footer';
-import { TooltipBottom } from '@/components/Tooltip';
 
 const ProfileContainer = styled(Box)`
   margin-top: 6em;
 `;
 
-const UserInfoContainer = styled(Box)`
-  display: inline-flex;
-  align-items: center;
-  margin-bottom: 2em;
-`;
-
-const UserAvatar = styled(Avatar)`
-  width: 8em;
-  height: 8em;
-  margin: 0 3em 0 2em;
-`;
-
-const UserActionIcon = ({ title, icon, count }) => (
-  <TooltipBottom title={title}>
-    <div
-      style={{
-        marginRight: '1em',
-        display: 'inline-block',
-        fontSize: '1.25em',
-      }}
-    >
-      {icon}
-      <span style={{ marginLeft: '0.35rem' }}>{count}</span>
-    </div>
-  </TooltipBottom>
-);
-
-const iconStyle = {
-  height: '2em',
-  width: '2em',
-};
-
-const UserIcons = ({ adoptedCount, likedCount, plantedCount }) => (
-  <Box sx={{ mb: 2 }}>
-    <UserActionIcon
-      title="Adopted"
-      count={adoptedCount}
-      icon={<AdoptionIcon style={iconStyle} />}
-    />
-    <UserActionIcon
-      title="Liked"
-      count={likedCount}
-      icon={<Star style={iconStyle} />}
-    />
-    <UserActionIcon
-      title="Planted"
-      count={plantedCount}
-      icon={
-        <img
-          alt=""
-          src={TreeIcon}
-          style={{ ...iconStyle, marginRight: '0.2em' }}
-        />
-      }
-    />
-  </Box>
-);
-
 export default function UserProfile() {
   const { user = {} } = useAuth0();
-  const { name, nickname, email, picture } = user;
+  const { nickname, email } = user;
   const emailQuery = { email };
   const emailEnabled = { enabled: !!email };
   const { data: adoptedTrees = [] } = useUserAdoptedQuery(
@@ -99,19 +38,12 @@ export default function UserProfile() {
   return (
     <>
       <ProfileContainer>
-        <UserInfoContainer>
-          <UserAvatar alt="Avatar" src={picture} />
-          <div>
-            <UserIcons
-              adoptedCount={adoptedTrees.length}
-              likedCount={likedTrees.length}
-              plantedCount={plantedTrees.length}
-            />
-            <Typography variant="body1">{name}</Typography>
-            <Typography variant="body1">{nickname}</Typography>
-            <Typography variant="body1">{email}</Typography>
-          </div>
-        </UserInfoContainer>
+        <UserInfoContainer
+          user={user}
+          adoptedTrees={adoptedTrees}
+          likedTrees={likedTrees}
+          plantedTrees={plantedTrees}
+        />
         <UserTreeHistoryTable rows={treeHistory} />
       </ProfileContainer>
       <Footer />
